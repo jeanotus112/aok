f802decebcea279be90ac66c5b11bba4cb4e4327
Zou Jiyun <zoujiyun@tinman.cn>
2021-02-11 18:53:05 +0800

    feat: 建立mysql->orm映射

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,8 +1,14 @@
-import { RouterContext } from '@koa/router';
+import { RouterContext } from "@koa/router";
+import { getManager } from "typeorm";
+import { User } from "../entity/user";
 
 export default class UserController {
   public static async listUsers(ctx: RouterContext) {
-    ctx.body = 'ListUsers controller';
+    const userRepository = getManager().getRepository(User);
+    const users = await userRepository.find();
+
+    ctx.status = 200;
+    ctx.body = users;
   }
 
   public static async showUserDetail(ctx: RouterContext) {
--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,37 @@
 // src/server.ts
-import Koa from 'koa';
-import cors from '@koa/cors';
-import bodyParser from 'koa-bodyparser';
-import { protectedRouter, unprotectedRouter } from './routes'
-import {logger} from './logger'
-import jwt from 'koa-jwt'
-import {JWT_SECRET} from './constants'
+import Koa from "koa";
+import cors from "@koa/cors";
+import bodyParser from "koa-bodyparser";
+import { protectedRouter, unprotectedRouter } from "./routes";
+import { logger } from "./logger";
+import jwt from "koa-jwt";
+import { JWT_SECRET } from "./constants";
+import { createConnection } from "typeorm";
 
-// 初始化 Koa 应用实例
-const app = new Koa();
+createConnection()
+  .then(() => {
+    // 初始化 Koa 应用实例
+    const app = new Koa();
+    // 注册中间件
+    app.use(logger());
+    app.use(cors());
+    app.use(bodyParser());
 
-// 注册中间件
-app.use(logger())
-app.use(cors());
-app.use(bodyParser());
+    app.use(async (ctx, next) => {
+      try {
+        await next();
+      } catch (err) {
+        // 只返回 JSON 格式的响应
+        ctx.status = err.status || 500;
+        ctx.body = { message: err.message };
+      }
+    });
 
-app.use(async (ctx, next) => {
-  try {
-    await next();
-  } catch (err) {
-    // 只返回 JSON 格式的响应
-    ctx.status = err.status || 500;
-    ctx.body = { message: err.message };
-  }
-});
+    app.use(unprotectedRouter.routes()).use(unprotectedRouter.allowedMethods());
+    app.use(jwt({ secret: JWT_SECRET }).unless({ method: "GET" }));
+    app.use(protectedRouter.routes()).use(protectedRouter.allowedMethods());
 
-app.use(unprotectedRouter.routes()).use(unprotectedRouter.allowedMethods());
-app.use(jwt({ secret: JWT_SECRET }).unless({ method: 'GET' }))
-app.use(protectedRouter.routes()).use(protectedRouter.allowedMethods());
-
-// 运行服务器
-app.listen(3000);
+    // 运行服务器
+    app.listen(3000);
+  })
+  .catch((err: string) => console.log("TypeORM connection error:", err));
